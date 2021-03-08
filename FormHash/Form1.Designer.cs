c9cb8098e67abe4adb151adae4158061dd4d39bc
HSH941121 <57350753+HSH941121@users.noreply.github.com>
2021-03-08 16:55:26 +0900

    Add files via upload

---
+++ FormHash/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace FormHash
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbName = new System.Windows.Forms.TextBox();
+            this.tbPhone = new System.Windows.Forms.TextBox();
+            this.btShow = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tbName
+            // 
+            this.tbName.Location = new System.Drawing.Point(41, 73);
+            this.tbName.Name = "tbName";
+            this.tbName.Size = new System.Drawing.Size(100, 21);
+            this.tbName.TabIndex = 0;
+            // 
+            // tbPhone
+            // 
+            this.tbPhone.Location = new System.Drawing.Point(263, 73);
+            this.tbPhone.Name = "tbPhone";
+            this.tbPhone.Size = new System.Drawing.Size(100, 21);
+            this.tbPhone.TabIndex = 0;
+            // 
+            // btShow
+            // 
+            this.btShow.Location = new System.Drawing.Point(169, 73);
+            this.btShow.Name = "btShow";
+            this.btShow.Size = new System.Drawing.Size(75, 23);
+            this.btShow.TabIndex = 1;
+            this.btShow.Text = ">>";
+            this.btShow.UseVisualStyleBackColor = true;
+            this.btShow.Click += new System.EventHandler(this.btShow_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(41, 55);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 12);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "이름";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(263, 55);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 12);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "전화번호";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btShow);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.tbName);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.tbPhone);
+            this.groupBox1.Location = new System.Drawing.Point(12, 53);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(401, 157);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "전화번호 탐색기";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(425, 439);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "전화번호부";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbName;
+        private System.Windows.Forms.TextBox tbPhone;
+        private System.Windows.Forms.Button btShow;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
+
---
+++ FormHash/Form1.cs
@@ -0,0 +1,45 @@
+﻿
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace FormHash
+{
+    public partial class Form1 : Form
+    {
+        Hashtable myTable;
+
+        public Form1()
+        {
+            InitializeComponent();
+            myTable = new Hashtable();
+
+            myTable["하선호"] = "010-0000-0000";
+            myTable["홍길동"] = "010-1111-1111";
+            
+        }
+
+        private void btShow_Click(object sender, EventArgs e)
+        {
+            if (myTable.Contains(tbName.Text))
+            {
+                tbPhone.Text = "";
+                tbPhone.Text = myTable[tbName.Text].ToString();
+            }
+            else
+            {
+                tbPhone.Text = "일치하는 사람이 없습니다.";
+            }
+
+            
+        }
+    }
+}
